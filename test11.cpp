9d0e9e04f0aee5dca6effe9b3b14f9b4951f2e32
SujalUshir <ushirthbsujal@gmail.com>
2026-03-06 09:31:26 +0530

    Create test11.cpp with variable examples and I/O
    Add a C++ program demonstrating variables, data types, and basic input/output operations.

---
+++ test11.cpp
@@ -0,0 +1,60 @@
+#include <stdio.h>
+/// @brief please solve this queestion ... FK U 
+/// @return 
+int main() {
+    printf("Helo, World!\n");
+    return 0;
+}
+
+@Variables and Data Types
+```c
+`#include <stdio.h>
+int main() {
+   int number;               //variable
+   int age = 22;             //constant
+   float pi = 3.14;          //decimal
+   char hashtag = '#';       //characternjnjnn
+}jijnknm
+```kkmkm
+@outputs
+```c
+#include<stdio.h>
+int main() {
+int age = 17;
+float pi = 3.14;
+char hashtag = '#';
+
+printf("age is %d\n", age);            //age is 17
+printf("value is %f\n", pi);           //value is 3.14        
+printf("charachter is %c\n", hashtag); //charahter is #
+return 0;
+}
+```
+@inputs
+```c
+inputs = scanf(" %d ", &age);
+# include <stdio.h>
+
+int main() {
+     int num1;
+     int num2 ;
+     printf("enter a number:"):
+     scanf("%d",&num1);
+     printf("enter another number:");
+     scanf("%d",&num2);
+     printf("sum of num 1 and num2 is: %d",num1+num2);
+     return 0;                                               //num1+num2 =ans
+}
+```
+**==Type of operators==**:
+a. Arithmetic Operators :-Normal multiplication and division tools
+b. Relational Operators :-To compare two values :-   1)== means both are equal
+                                         2)>, >=   (3) <, <=   (4) !=
+c. Logical Operators:-   1) && :- AND
+                    2) || :- OR
+                    3) ! :- NOT
+d. Bitwise Operators
+e. Assignment Operators
+f. Ternary Operator
+//asdfghjkhgfdsdfghjklkjhgfdsasdfghjk
+____________________________________________________________________
